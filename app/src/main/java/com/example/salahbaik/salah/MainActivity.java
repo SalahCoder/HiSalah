ae572e6bf5af2af3df88116a06cde3c75e479627
salahbaik <ssbeack@gmail.com>
2018-03-11 01:43:02 +0300

    i have add new java file, its called firstTest

--- app/src/main/java/com/example/salahbaik/salah/MainActivity.java
+++ app/src/main/java/com/example/salahbaik/salah/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
+// this is new comment just only for testting
 
 }
---
+++ app/src/main/java/com/example/salahbaik/salah/firstTest.java
@@ -0,0 +1,8 @@
+package com.example.salahbaik.salah;
+
+/**
+ * Created by salahbaik on 3/11/18.
+ */
+
+public class firstTest {
+}
